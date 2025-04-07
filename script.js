ce5b8aef0fa5a4fd785c985ae21563dd966d842d
Matt Stein <matt.m.stein@gmail.com>
2025-04-07 17:20:12 -0400

    Sometimes a refactor is in order
    I tried updating the JavaScript variables names and also added comments explaining the general flow of the code. Some HTML was removed that was no longer needed.

--- script.js
+++ script.js
@@ -1,130 +1,207 @@
-const todoToday = document.getElementById("tasks-for-today");
-const todoLeftOver = document.getElementById("left-over-tasks");
+// The different elements brought in from the HTML using their specific ID
 const input = document.getElementById("task-add");
-const button = document.getElementById("submit-button");
-const todayList = document.getElementById("today-list");
-const laterTodos = document.getElementById("left-over-list");
+const submitButton = document.getElementById("submit-button");
+const todayUlElement = document.getElementById("today-list");
+const laterUlElement = document.getElementById("later-list");
 
-const todoFromStorage = JSON.parse(localStorage.getItem("todoToday"));
-const laterFromStorage = JSON.parse(localStorage.getItem("todoLater"));
+// GLOBAL VARIABLES
+let todayTaskList = [];
+let laterTaskList = [];
 
-let todos = [];
-let laterList = [];
+function startUpScript() {
+  // Retrive stored data that is stored on the local computer's webbrowser
+  const todaysTasksFromStorage = JSON.parse(
+    localStorage.getItem("todaysTasks")
+  );
+  const latersTasksFromStorage = JSON.parse(
+    localStorage.getItem("latersTasks")
+  );
 
-if (todoFromStorage != null && todoFromStorage.length > 0) {
-  todos = todoFromStorage;
-  populateList("Today");
-}
-
-if (laterFromStorage != null && laterFromStorage.length > 0) {
-  laterList = laterFromStorage;
-  populateList("Later");
-}
-
-input.addEventListener("keyup", (event) => {
-  if (event.key == "Enter") {
-    addTask();
+  // Checks to see if either list is stored in local storage
+  if (isStoredListValid(todaysTasksFromStorage)) {
+    todayTaskList = todaysTasksFromStorage;
   }
-});
 
-button.addEventListener("click", (event) => {
-  addTask();
-});
+  if (isStoredListValid(latersTasksFromStorage)) {
+    laterTaskList = latersTasksFromStorage;
+  }
 
+  // Populates all the task lists if there is any valid data in local storage
+  populateAllLists();
+
+  // Whenever the enter key is press, add a task
+  input.addEventListener("keyup", (event) => {
+    if (event.key == "Enter") {
+      addTask();
+    }
+  });
+
+  // If the submit button is clicked, add the task.
+  submitButton.addEventListener("click", (event) => {
+    addTask();
+  });
+}
+
+// Checks to see if there is valid data obtained from local storage
+function isStoredListValid(storedList) {
+  // Check to see if the list is null...
+  const condition1 = storedList != null;
+
+  // If it is, make sure not to proceed as not not throw a null object error
+  if (!condition1) {
+    return false;
+  }
+
+  // The next condition is to make sure if it has a length, it is greater than 0
+  const condition2 = storedList.length > 0;
+
+  // Only return true when both conditions are true.
+  return condition1 && condition2;
+}
+
+// Adds a task to the today list
 function addTask() {
-  todos.push(input.value);
+  const task = input.value;
+  if (task == "") {
+    return;
+  }
+
+  // Push the task to the today task list array
+  todayTaskList.push(task);
+
+  // The only thing that could have changed is the Today list
   populateList("Today");
+
+  // Return the input element to its intiial value
   input.value = "";
+
+  // Updates local storage
   storeItems();
 }
 
-function moveTask(task, list) {
-  if (list == "Today") {
-    laterList.push(task);
-    console.log(laterList);
-  } else if (list == "Later") {
-    todos.push(task);
-  }
+// Populates both the today list and the later list for now
+function populateAllLists() {
   populateList("Today");
   populateList("Later");
 }
 
+// Given a task it moves it to opposite list
+function moveTaskToList(task, list) {
+  // If the task comes from the "Today" list, mmove it to the later
+  if (list == "Today") {
+    laterTaskList.push(task);
+  } else if (list == "Later") {
+    // Or do the exact opposite
+    todayTaskList.push(task);
+  }
+}
+
+// Populates a specific list
 function populateList(list) {
-  console.log("Here");
+  // These are the common variables
   let toInsertList = [];
   let toInsertTodos = [];
   let listIconClassname = "";
 
+  // Sets the correct list and appropriate variables
   if (list == "Today") {
-    toInsertList = todayList;
-    toInsertTodos = todos;
+    toInsertList = todayUlElement;
+    toInsertTodos = todayTaskList;
     listIconClassname = "fa-up-long";
   } else if (list == "Later") {
-    toInsertList = laterTodos;
-    toInsertTodos = laterList;
+    toInsertList = laterUlElement;
+    toInsertTodos = laterTaskList;
     listIconClassname = "fa-down-long";
   } else {
     console.log("Error: Wrong List");
   }
 
-  toInsertList.innerHTML = "";
+  // Return the list elements DOM back to its blank state.
+  toInsertList.innerHTML = ``;
+
+  // Loop through the task list array with the item and an index.
   toInsertTodos.forEach((todo, index) => {
+    // Create both the container and inner container elements
     const itemElement = document.createElement("li");
     itemElement.innerText = todo;
     const iconDiv = document.createElement("div");
     itemElement.appendChild(iconDiv);
 
+    // Create the delet button with the right classes
     const deleteButton = document.createElement("i");
     deleteButton.classList.add("delete-button");
     deleteButton.classList.add("fa-solid");
     deleteButton.classList.add("fa-trash");
     deleteButton.classList.add("item-icon");
 
+    // Create the move icon with the right classes
     const moveIcon = document.createElement("i");
     moveIcon.classList.add("fa-solid");
     moveIcon.classList.add(listIconClassname);
     moveIcon.classList.add("item-icon");
 
+    // Add an event listener waiting for the user to click to the delete icon
     deleteButton.addEventListener("click", (event) => {
+      // Remove the item, update local storage, and update both lists
       removeItem(index, list);
       storeItems();
-      populateList("Today");
-      populateList("Later");
+      populateAllLists();
     });
 
+    // Listen for the click event from the move icon
     moveIcon.addEventListener("click", (event) => {
+      // Move the item to the other listt, remove it from its current list, and update local storage.
+      moveTaskToList(todo, list);
       removeItem(index, list);
-      moveTask(todo, list);
       storeItems();
+
+      // Then populate all lists
+      populateAllLists();
     });
 
+    // Append both icons within the icon list.
     iconDiv.appendChild(moveIcon);
     iconDiv.appendChild(deleteButton);
+
+    // Add the new task to the appropriate list.
     toInsertList.appendChild(itemElement);
   });
 }
 
+// Given the index remove it from the passed in list
 function removeItem(i, list) {
+  // Temporarily holds the data from the passed in list
   let tempTodo = [];
 
   if (list == "Today") {
-    todos.forEach((item, index) => {
+    // Loop through the today task list and unless it is the passed on
+    // i, add it to the temporary storage array.
+    todayTaskList.forEach((item, index) => {
       if (index != i) {
         tempTodo.push(item);
       }
     });
-    todos = tempTodo;
+
+    // Updates the today list without the removed element.
+    todayTaskList = tempTodo;
   } else if (list == "Later") {
-    laterList.forEach((item, index) => {
+    laterTaskList.forEach((item, index) => {
+      // Exact same functionality as the "Today" list
       if (index != i) {
         tempTodo.push(item);
       }
     });
-    laterList = tempTodo;
+
+    // Updates the later list without the removed element.
+    laterTaskList = tempTodo;
   }
 }
 
+// Update local storage for both lists with the correctly formated task list
 function storeItems() {
-  localStorage.setItem("todoToday", JSON.stringify(todos));
-  localStorage.setItem("todoLater", JSON.stringify(laterList));
+  localStorage.setItem("todaysTasks", JSON.stringify(todayTaskList));
+  localStorage.setItem("latersTasks", JSON.stringify(laterTaskList));
 }
+
+// Run the startup script
+startUpScript();
