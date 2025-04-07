bb5e9acbad563456bcef141fa4e9292a7ad6766e
Matt Stein <matt.m.stein@gmail.com>
2025-04-06 21:05:24 -0400

    Now fixes the delete task in the later list error
    There was a problem with the code resulting in tasks on the later list not being deleted when the icon was clicked which is now fixed.

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # todo-today
- This todo list app allows you to add tasks for the day and carry them over to the next day.
+
+This todo list app allows you to add tasks for the day and carry them over to the next day.
+
+# Tasks
+
+[X] - Get the basics of the today list working
+
+[X] - Allow deleting of tasks with the click of an icon
+
+[X] - Store tasks in the browser's local storage
+
+[X] - Add basic functionality to the later list
+
+[X] - Make the ability to delete tasks in the later list
+
+[ ] - Add the ability to move tasks back down to the today list
+
+[ ] - Implement an automatic moving of the tasks to the later task list
+
+[ ] - Choose which time of day for the tasks to automatically move to the later list
+
+[ ] - Make an animation for deleting a task
+
+[ ] - Animate the icons way better
+
+[ ] - Make an animation for moving the task between lists
--- script.js
+++ script.js
@@ -1,123 +1,123 @@
-const todoToday = document.getElementById('tasks-for-today');
-const todoLeftOver = document.getElementById('left-over-tasks');
-const input = document.getElementById('task-add');
-const button = document.getElementById('submit-button');
-const todayList = document.getElementById('today-list');
-const laterTodos = document.getElementById('left-over-list');
+const todoToday = document.getElementById("tasks-for-today");
+const todoLeftOver = document.getElementById("left-over-tasks");
+const input = document.getElementById("task-add");
+const button = document.getElementById("submit-button");
+const todayList = document.getElementById("today-list");
+const laterTodos = document.getElementById("left-over-list");
 
-const todoFromStorage = JSON.parse(localStorage.getItem('todoToday'));
-const laterFromStorage = JSON.parse(localStorage.getItem('todoLater'));
+const todoFromStorage = JSON.parse(localStorage.getItem("todoToday"));
+const laterFromStorage = JSON.parse(localStorage.getItem("todoLater"));
 
 let todos = [];
 let laterList = [];
 
 if (todoFromStorage != null && todoFromStorage.length > 0) {
-    todos = todoFromStorage;
-    populateList("Today");
+  todos = todoFromStorage;
+  populateList("Today");
 }
 
 if (laterFromStorage != null && laterFromStorage.length > 0) {
-    laterList = laterFromStorage;
-    populateList('Later');
+  laterList = laterFromStorage;
+  populateList("Later");
 }
 
-input.addEventListener('keyup', (event) => {
-    if (event.key == 'Enter') {
-        addTask();
-    }
+input.addEventListener("keyup", (event) => {
+  if (event.key == "Enter") {
+    addTask();
+  }
 });
 
-button.addEventListener('click', (event) => {
-    addTask();
+button.addEventListener("click", (event) => {
+  addTask();
 });
 
 function addTask() {
-    todos.push(input.value);
-    populateList("Today");
-    input.value = '';
-    storeItems();
+  todos.push(input.value);
+  populateList("Today");
+  input.value = "";
+  storeItems();
 }
 
 function moveTaskUp(task) {
-    laterList.push(task);
-    populateList('Today');
-    populateList('Later');
-
+  laterList.push(task);
+  populateList("Today");
+  populateList("Later");
 }
 
 function populateList(list) {
-    console.log("Here")
-    let toInsertList = [];
-    let toInsertTodos = [];
+  console.log("Here");
+  let toInsertList = [];
+  let toInsertTodos = [];
 
-    if (list == "Today") {
-        toInsertList = todayList;
-        toInsertTodos = todos;
-    } else if (list == "Later") {
-        toInsertList = laterTodos;
-        toInsertTodos = laterList;
-    } else {
-        console.log("Error: Wrong List")
-    }
+  if (list == "Today") {
+    toInsertList = todayList;
+    toInsertTodos = todos;
+  } else if (list == "Later") {
+    toInsertList = laterTodos;
+    toInsertTodos = laterList;
+  } else {
+    console.log("Error: Wrong List");
+  }
 
-    toInsertList.innerHTML = '';
+  toInsertList.innerHTML = "";
 
-    toInsertTodos.forEach((todo, index) => {
-        const itemElement = document.createElement('li');
-        itemElement.innerText = todo;
-        const iconDiv = document.createElement('div');
-        itemElement.appendChild(iconDiv);
-    
-        const deleteButton = document.createElement('i');
-        deleteButton.classList.add('delete-button');
-        deleteButton.classList.add('fa-solid');
-        deleteButton.classList.add('fa-trash');
-        deleteButton.classList.add('item-icon');
+  toInsertTodos.forEach((todo, index) => {
+    const itemElement = document.createElement("li");
+    itemElement.innerText = todo;
+    const iconDiv = document.createElement("div");
+    itemElement.appendChild(iconDiv);
 
-        const upIcon = document.createElement('i');
-        upIcon.classList.add('fa-solid');
-        upIcon.classList.add('fa-up-long');
-        upIcon.classList.add('item-icon');
+    const deleteButton = document.createElement("i");
+    deleteButton.classList.add("delete-button");
+    deleteButton.classList.add("fa-solid");
+    deleteButton.classList.add("fa-trash");
+    deleteButton.classList.add("item-icon");
 
-        deleteButton.addEventListener('click', (event) => {
-            removeItem(index, list);
-            storeItems();
-            populateList("Today");
-        });
+    const upIcon = document.createElement("i");
+    upIcon.classList.add("fa-solid");
+    upIcon.classList.add("fa-up-long");
+    upIcon.classList.add("item-icon");
 
-        upIcon.addEventListener('click', (event) => {
-            removeItem(index, "Today");
-            moveTaskUp(todo);
-            storeItems();
-        });
+    deleteButton.addEventListener("click", (event) => {
+      removeItem(index, list);
+      storeItems();
+      populateList("Today");
+      populateList("Later");
+    });
+
+    upIcon.addEventListener("click", (event) => {
+      removeItem(index, "Today");
+      moveTaskUp(todo);
+      storeItems();
+    });
 
     iconDiv.appendChild(upIcon);
     iconDiv.appendChild(deleteButton);
     toInsertList.appendChild(itemElement);
-    });
+  });
 }
 
 function removeItem(i, list) {
-    let tempTodo = [];
+  let tempTodo = [];
 
-    if (list == "Today") {
-        todos.forEach((item, index) => {
-            if (index != i) {
-                tempTodo.push(item);
-            }
-        });
-        todos = tempTodo;
-    } else if (list == "Later") {
-        laterTodos.forEach((item, index) => {
-            if (index != i) {
-                tempTodo.push(item);
-            }
-        });
-        laterTodos = tempTodo;
-    }
+  if (list == "Today") {
+    todos.forEach((item, index) => {
+      if (index != i) {
+        tempTodo.push(item);
+      }
+    });
+    todos = tempTodo;
+  } else if (list == "Later") {
+    laterList.forEach((item, index) => {
+      if (index != i) {
+        tempTodo.push(item);
+      }
+    });
+    laterList = tempTodo;
+  }
 }
 
 function storeItems() {
-    localStorage.setItem('todoToday', JSON.stringify(todos));
-    localStorage.setItem('todoLater', JSON.stringify(laterList));
+  localStorage.setItem("todoToday", JSON.stringify(todos));
+  localStorage.setItem("todoLater", JSON.stringify(laterList));
 }
